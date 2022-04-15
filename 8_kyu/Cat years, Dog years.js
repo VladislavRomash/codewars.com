f2695b674870875ab0a661a1d42c6280df620fce
VladislavRomash <romashkanvlad@gmail.com>
2022-04-15 11:04:44 +0300

    A solved task has been added

---
+++ 8_kyu/Cat years, Dog years.js
@@ -0,0 +1,14 @@
+var humanYearsCatYearsDogYears = function (humanYears) {
+    switch (humanYears) {
+        case 1:
+            return [1, 15, 15]
+        case 2:
+            return [2, 24, 24]
+        default:
+            return [humanYears, (24 + ((humanYears - 2) * 4)), (24 + ((humanYears - 2) * 5))]
+    }
+}
+
+humanYearsCatYearsDogYears(1)
+humanYearsCatYearsDogYears(2)
+humanYearsCatYearsDogYears(10)
