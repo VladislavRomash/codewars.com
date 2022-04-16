6e477d56d70f481e9723d84a2520d3e0828269ad
VladislavRomash <romashkanvlad@gmail.com>
2022-04-16 10:44:41 +0300

    A solved task has been added

---
+++ 8_kyu/My head is at the wrong end.js
@@ -0,0 +1,7 @@
+function fixTheMeerkat(arr) {
+    return arr.reverse()
+}
+
+fixTheMeerkat(["tail", "body", "head"])
+fixTheMeerkat(["tails", "body", "heads"])
+fixTheMeerkat(["bottom", "middle", "top"])
