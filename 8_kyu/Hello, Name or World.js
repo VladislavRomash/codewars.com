52095cc59b0be314ccf2fb865d8b345960a6053a
VladislavRomash <romashkanvlad@gmail.com>
2022-04-19 12:26:23 +0300

    A solved task has been added

---
+++ 8_kyu/Hello, Name or World.js
@@ -0,0 +1,8 @@
+function hello(name) {
+    return name === '' || name === undefined ? 'Hello, World!' : `Hello, ${name[0].toUpperCase()}${name.slice(1).toLowerCase()}!`
+}
+
+hello('johN')
+hello('alice')
+hello()
+hello('')
