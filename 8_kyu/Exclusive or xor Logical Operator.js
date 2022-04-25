50141a07521e34aae41c627e1622fbea3f3ffcb5
VladislavRomash <romashkanvlad@gmail.com>
2022-04-25 16:33:34 +0300

    A solved task has been added

---
+++ 8_kyu/Exclusive or xor Logical Operator.js
@@ -0,0 +1,9 @@
+function xor(a, b) {
+    return a !== b
+}
+
+xor(false, false)
+xor(true, false)
+
+xor(true, true) //false, "true xor true === false"
+xor(true, true) // true, "'xor' is NOT identical to 'or'"
