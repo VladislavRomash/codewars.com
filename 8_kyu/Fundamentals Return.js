de6989e9cd7ed457d92d1f8c71653a8f52da8732
VladislavRomash <romashkanvlad@gmail.com>
2022-04-30 09:36:00 +0300

    A solved task has been added

---
+++ 8_kyu/Fundamentals Return.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b
+}
+
+function divide(a, b) {
+    return a / b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+function mod(a, b) {
+    return a % b
+}
+
+function exponent(a, b) {
+    return Math.pow(a, b)
+}
+
+function subt(a, b) {
+    return a - b
+}
+
+add(1, 2)
+multiply(1, 2)
+divide(2, 1)
+mod(1, 2)
+exponent(1, 2)
+subt(1, 2)
