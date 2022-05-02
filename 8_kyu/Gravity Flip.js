959eeb449529138076d373fec91f0218ffb7f5fc
VladislavRomash <romashkanvlad@gmail.com>
2022-05-02 11:06:15 +0300

    A solved task has been added

---
+++ 8_kyu/Gravity Flip.js
@@ -0,0 +1,12 @@
+const flip = (d, a) => {
+    if (d === 'R') {
+        a.sort((a, b) => a > b ? 1 : -1)
+    }
+    if (d === 'L') {
+        a.sort((a, b) => a < b ? 1 : -1)
+    }
+    return a
+}
+
+flip('R', [3, 2, 1, 2])
+flip('L', [1, 4, 5, 3, 5])
