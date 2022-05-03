e90e835aa21e55293c791149bb9e2e7249d99183
VladislavRomash <romashkanvlad@gmail.com>
2022-05-03 12:04:51 +0300

    A solved task has been added

---
+++ 8_kyu/Fix your code before the garden dies.js
@@ -0,0 +1,10 @@
+function rainAmount(mm) {
+    if (mm < 40) {
+        return `You need to give your plant ${40 - mm}mm of water`
+    } else {
+        return "Your plant has had more than enough water for today!"
+    }
+}
+
+rainAmount(100)
+rainAmount(39)
