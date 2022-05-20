1f87f26bd3a38eed502478959424d5114ed60d74
VladislavRomash <romashkanvlad@gmail.com>
2022-05-20 11:13:32 +0300

    A solved task has been added

---
+++ 8_kyu/Leonardo Dicaprio and Oscars.js
@@ -0,0 +1,9 @@
+function leo(oscar) {
+    if (oscar === 88) return `Leo finally won the oscar! Leo is happy`
+    if (oscar > 88) return `Leo got one already!`
+    if (oscar === 86) return `Not even for Wolf of wallstreet?!`
+    else return `When will you give Leo an Oscar?`
+}
+
+leo(89)
+leo(87)
