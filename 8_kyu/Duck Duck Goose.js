a3ccb0042fdb85b24a72b729c0012a2353c807e7
VladislavRomash <romashkanvlad@gmail.com>
2022-05-20 11:04:50 +0300

    A solved task has been added

---
+++ 8_kyu/Duck Duck Goose.js
@@ -0,0 +1,16 @@
+class Player {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+let ex_names = ["a", "b", "c", "d", "c", "e", "f", "g", "h", "z"];
+let players = ex_names.map((n) => new Player(n));
+
+
+function duckDuckGoose(players, goose) {
+    return players[(goose - 1) % players.length].name;
+}
+
+
+duckDuckGoose(players, 1)
