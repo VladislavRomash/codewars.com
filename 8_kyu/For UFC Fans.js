042f20d47026389d61dbe818e81ae9f5af86e982
VladislavRomash <romashkanvlad@gmail.com>
2022-05-21 09:43:21 +0300

    A solved task has been added

---
+++ 8_kyu/For UFC Fans.js
@@ -0,0 +1,6 @@
+const quote = function (fighter) {
+    return fighter.toLowerCase() === 'conor mcgregor' ? `I'd like to take this chance to apologize.. To absolutely NOBODY!` : `I am not impressed by your performance.`
+};
+
+quote('george saint pierre')
+quote('conor mcgregor')
