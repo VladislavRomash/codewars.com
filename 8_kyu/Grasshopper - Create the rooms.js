9b6bb4a6d6c527bc1568ef4d71b3cb9412452ed6
VladislavRomash <romashkanvlad@gmail.com>
2022-06-03 09:47:21 +0300

    A solved task has been added

---
+++ 8_kyu/Grasshopper - Create the rooms.js
@@ -0,0 +1,17 @@
+const rooms = {
+    first: {
+        name: 'first',
+        description: 'firstRoom',
+        completed: true,
+    },
+    second: {
+        name: 'second',
+        description: 'secondRoom',
+        completed: false,
+    },
+    third: {
+        name: 'third',
+        description: 'thirdRoom',
+        completed: false,
+    }
+}
