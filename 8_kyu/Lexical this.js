3b950e246baef82d5d050c9c3c897a5861860f5a
VladislavRomash <romashkanvlad@gmail.com>
2022-09-01 10:09:52 +0300

    A solved task has been added

---
+++ 8_kyu/Lexical this.js
@@ -0,0 +1,11 @@
+var Person = function () {
+    var person = {
+        _name: "Leroy",
+        _friends: [],
+        fillFriends(f) {
+            this._friends.push(...f)
+        }
+    }
+    return person;
+}
+
