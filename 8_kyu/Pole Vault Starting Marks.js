26baec20bd5a6ac77d1c76988812a5cf39f008b8
VladislavRomash <romashkanvlad@gmail.com>
2022-09-02 21:38:03 +0300

    A solved task has been added

---
+++ 8_kyu/Pole Vault Starting Marks.js
@@ -0,0 +1,8 @@
+function startingMark(bodyHeight) {
+    const m = (10.67 - 9.45) / (1.83 - 1.52)
+    return Math.round((10.67 + m * bodyHeight - m * 1.83) * 100) / 100
+}
+
+startingMark(1.52)
+startingMark(1.83)
+startingMark(1.22)
