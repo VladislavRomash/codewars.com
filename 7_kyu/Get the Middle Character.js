471613ba26c5d2904badbda811131018380c148e
VladislavRomash <romashkanvlad@gmail.com>
2022-09-06 15:00:10 +0300

    A solved task has been added

---
+++ 7_kyu/Get the Middle Character.js
@@ -0,0 +1,7 @@
+function getMiddle(s) {
+    return s.length % 2 === 0 ? `${s[(s.length / 2) - 1]}${s[s.length / 2]}` : s[Math.floor(s.length / 2)]
+}
+
+getMiddle("test")
+getMiddle("testing")
+getMiddle("middle")
