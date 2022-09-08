82490335b0d23aef9a77962c11a373141054d165
VladislavRomash <romashkanvlad@gmail.com>
2022-09-08 14:24:16 +0300

    Added alternative solution for task

--- 7_kyu/Complementary DNA.js
+++ 7_kyu/Complementary DNA.js
@@ -11,3 +11,21 @@ function DNAStrand(dna) {
 DNAStrand("AAAA")
 DNAStrand("ATTGC")
 DNAStrand("GTAT")
+
+
+/*
+function DNAStrand(dna) {
+    let str = ''
+    for (let i = 0; i < dna.length; i++) {
+        if (dna[i] === 'A') {
+            str += 'T'
+        } else if (dna[i] === 'T') {
+            str += 'A'
+        } else if (dna[i] === 'C') {
+            str += 'G'
+        } else if (dna[i] === 'G') {
+            str += 'C'
+        }
+    }
+    return str
+}*/
