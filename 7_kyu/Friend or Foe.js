74df5a163db90370713e35a480c20db97e397b08
VladislavRomash <romashkanvlad@gmail.com>
2022-09-09 12:00:10 +0300

    A solved task has been added

---
+++ 7_kyu/Friend or Foe.js
@@ -0,0 +1,7 @@
+function friend(friends) {
+    return friends.filter(f => f.length === 4)
+}
+
+friend(["Ryan", "Kieran", "Mark"])
+friend(["Ryan", "Jimmy", "123", "4", "Cool Man"])
+friend(["Jimm", "Cari", "aret", "truehdnviegkwgvke", "sixtyiscooooool"])
