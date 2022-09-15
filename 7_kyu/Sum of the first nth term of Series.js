1a1e059dd4811856803d3d415f398ffec2ea5ebf
VladislavRomash <romashkanvlad@gmail.com>
2022-09-15 11:20:17 +0300

    A solved task has been added

---
+++ 7_kyu/Sum of the first nth term of Series.js
@@ -0,0 +1,11 @@
+function SeriesSum(n) {
+    let sum = 0
+    for (let i = 0; i < n; i++) {
+        sum += 1 / (1 + i * 3)
+    }
+    return sum.toFixed(2)
+}
+
+SeriesSum(1)
+SeriesSum(2)
+SeriesSum(3)
