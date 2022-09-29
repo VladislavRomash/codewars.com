ddfef992bb5a7b85e20e1df55a8a6358665cc9ee
VladislavRomash <romashkanvlad@gmail.com>
2022-09-29 12:37:21 +0300

    A solved task has been added

---
+++ 7_kyu/Sum of Minimums.js
@@ -0,0 +1,6 @@
+function sumOfMinimums(arr) {
+    return arr.map(m => Math.min(...m)).reduce((a, b) => a + b)
+}
+
+sumOfMinimums([[7, 9, 8, 6, 2], [6, 3, 5, 4, 3], [5, 8, 7, 4, 5]])
+sumOfMinimums([[11, 12, 14, 54], [67, 89, 90, 56], [7, 9, 4, 3], [9, 8, 6, 7]])
