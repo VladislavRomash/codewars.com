8567a21995a7d3a97af07517b5ad3af7def01a27
VladislavRomash <romashkanvlad@gmail.com>
2022-10-17 14:43:48 +0300

    A solved task has been added

---
+++ 7_kyu/Speed Control.js
@@ -0,0 +1,10 @@
+function gps(s, x) {
+    let arr = []
+    if (x.length <= 1) return 0
+    for (let i = 0; i < x.length - 1; i++) {
+        arr.push((x[i + 1] - x[i]) * 3600 / s)
+    }
+    return Math.max(...arr)
+}
+
+
