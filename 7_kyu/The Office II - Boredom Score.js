0acc45940ee1db58c46c8d21234500a6129d293f
VladislavRomash <romashkanvlad@gmail.com>
2022-11-30 13:40:51 +0300

    A solved task has been added

---
+++ 7_kyu/The Office II - Boredom Score.js
@@ -0,0 +1,39 @@
+function boredom(staff) {
+    const score = {
+        accounts: 1,
+        finance: 2,
+        canteen: 10,
+        regulation: 3,
+        trading: 6,
+        change: 6,
+        IS: 8,
+        retail: 5,
+        cleaning: 4,
+        'pissing about': 25,
+    }
+    let total = 0
+    for (const key in staff) {
+        total += score[staff[key]]
+    }
+    return total <= 80 ? 'kill me now' : total < 100 && total > 80 ? 'i can handle this' : 'party time!!'
+}
+
+
+boredom({
+    tim: 'change', jim: 'accounts',
+    randy: 'canteen', sandy: 'change', andy: 'change', katie: 'IS',
+    laura: 'change', saajid: 'IS', alex: 'trading', john: 'accounts',
+    mr: 'finance'
+})
+boredom({
+    tim: 'IS', jim: 'finance',
+    randy: 'pissing about', sandy: 'cleaning', andy: 'cleaning',
+    katie: 'cleaning', laura: 'pissing about', saajid: 'regulation',
+    alex: 'regulation', john: 'accounts', mr: 'canteen'
+})
+boredom({
+    tim: 'accounts', jim: 'accounts',
+    randy: 'pissing about', sandy: 'finance', andy: 'change',
+    katie: 'IS', laura: 'IS', saajid: 'canteen', alex: 'pissing about',
+    john: 'retail', mr: 'pissing about'
+})
