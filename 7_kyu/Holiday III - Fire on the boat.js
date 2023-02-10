cedd0ddc1dd639d7d49caa33bd4ff52ce4a1d10a
VladislavRomash <romashkanvlad@gmail.com>
2023-02-10 17:13:37 +0300

    A solved task has been added

---
+++ 7_kyu/Holiday III - Fire on the boat.js
@@ -0,0 +1,7 @@
+function fireFight(s) {
+    return s.replace(/Fire/g, '~~')
+}
+
+fireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast")
+fireFight("Mast Deck Engine Water Fire")
+fireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain")
