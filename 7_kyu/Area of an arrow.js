ab0b0f70a28f3fba63fccf7701869251a1cecf53
VladislavRomash <romashkanvlad@gmail.com>
2023-02-28 11:24:04 +0300

    A solved task has been added

---
+++ 7_kyu/Area of an arrow.js
@@ -0,0 +1,8 @@
+function arrowArea(a, b) {
+    const triangleOneArea = 0.5 * (a / 2) * (b / 2)
+    const triangleTwoArea = 0.5 * (a / 2) * (b / 2)
+    return triangleOneArea + triangleTwoArea
+}
+
+arrowArea(4, 2)
+arrowArea(7, 6)
